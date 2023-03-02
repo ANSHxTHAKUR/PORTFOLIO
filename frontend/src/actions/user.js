19245657fbab0cd5cdcbbf0a5c6a9ec8dbf0b31c
ANSHxTHAKUR <sengaransh094@gmail.com>
2023-03-02 22:22:06 +0530

    "Changes Updated"

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -6,7 +6,7 @@ export const getUser = () => async (dispatch) => {
       type: "GET_USER_REQUEST",
     });
 
-    const { data } = await axios.get("/api/v1/user");
+    const { data } = await axios.get("https://ansh-sengar.onrender.com/api/v1/user");
 
     dispatch({
       type: "GET_USER_SUCCESS",
@@ -57,7 +57,7 @@ export const logout = () => async (dispatch) => {
       type: "LOGOUT_REQUEST",
     });
 
-    const { data } = await axios.get("/api/v1/logout");
+    const { data } = await axios.get("https://ansh-sengar.onrender.com/api/v1/logout");
 
     dispatch({
       type: "LOGOUT_SUCCESS",
@@ -77,7 +77,7 @@ export const loadUser = () => async (dispatch) => {
       type: "LOAD_USER_REQUEST",
     });
 
-    const { data } = await axios.get("/api/v1/me");
+    const { data } = await axios.get("https://ansh-sengar.onrender.com/api/v1/me");
 
     dispatch({
       type: "LOAD_USER_SUCCESS",
@@ -99,7 +99,7 @@ export const updateUser =
       });
 
       const { data } = await axios.put(
-        "/api/v1/admin/update",
+        "https://ansh-sengar.onrender.com/api/v1/admin/update",
         {
           name,
           email,
@@ -133,7 +133,7 @@ export const addTimeline = (title, description, date) => async (dispatch) => {
     });
 
     const { data } = await axios.post(
-      "/api/v1/admin/timeline/add",
+      "https://ansh-sengar.onrender.com/api/v1/admin/timeline/add",
       {
         title,
         description,
@@ -164,7 +164,7 @@ export const deleteTimeline = (id) => async (dispatch) => {
       type: "DELETE_TIMELINE_REQUEST",
     });
 
-    const { data } = await axios.delete(`/api/v1/admin/timeline/${id}`);
+    const { data } = await axios.delete(`https://ansh-sengar.onrender.com/api/v1/admin/timeline/${id}`);
 
     dispatch({
       type: "DELETE_TIMELINE_SUCCESS",
@@ -185,7 +185,7 @@ export const addYoutube = (title, url, image) => async (dispatch) => {
     });
 
     const { data } = await axios.post(
-      "/api/v1/admin/youtube/add",
+      "https://ansh-sengar.onrender.com/api/v1/admin/youtube/add",
       { title, url, image },
       {
         headers: {
@@ -212,7 +212,7 @@ export const deleteYoutube = (id) => async (dispatch) => {
       type: "DELETE_YOUTUBE_REQUEST",
     });
 
-    const { data } = await axios.delete(`/api/v1/admin/youtube/${id}`);
+    const { data } = await axios.delete(`https://ansh-sengar.onrender.com/api/v1/admin/youtube/${id}`);
 
     dispatch({
       type: "DELETE_YOUTUBE_SUCCESS",
@@ -234,7 +234,7 @@ export const addProject =
       });
 
       const { data } = await axios.post(
-        "/api/v1/admin/project/add",
+        "https://ansh-sengar.onrender.com/api/v1/admin/project/add",
         { title, url, image, description, techStack },
         {
           headers: {
@@ -261,7 +261,7 @@ export const deleteProject = (id) => async (dispatch) => {
       type: "DELETE_PROJECT_REQUEST",
     });
 
-    const { data } = await axios.delete(`/api/v1/admin/project/${id}`);
+    const { data } = await axios.delete(`https://ansh-sengar.onrender.com/api/v1/admin/project/${id}`);
 
     dispatch({
       type: "DELETE_PROJECT_SUCCESS",
@@ -282,7 +282,7 @@ export const contactUs = (name, email, message) => async (dispatch) => {
     });
 
     const { data } = await axios.post(
-      "/api/v1/contact",
+      "https://ansh-sengar.onrender.com/api/v1/contact",
       { name, email, message },
       {
         headers: {
